29b0b16b5dc2957adc8b9471378910a6c0652d58
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-24 01:30:42 +0300

    Property

---
+++ Proje_OPP(C#_Katmanlı_Mimari)/Entity/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Proje_OPP_C__Katmanlı_Mimari_.Entity
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Proje_OPP(C#_Katmanlı_Mimari)/Ornekler/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Proje_OPP_C__Katmanlı_Mimari_.Ornekler
+{
+    public class Class1
+    {
+    }
+}
