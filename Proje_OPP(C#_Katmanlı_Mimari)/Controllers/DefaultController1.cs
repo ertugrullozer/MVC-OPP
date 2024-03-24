10af2735a9b0e9485442b51796c69fe1da540077
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-24 12:58:44 +0300

    Paremetre Kullanımı

--- Proje_OPP(C#_Katmanlı_Mimari)/Controllers/DefaultController1.cs
+++ Proje_OPP(C#_Katmanlı_Mimari)/Controllers/DefaultController1.cs
@@ -36,10 +36,50 @@ namespace Proje_OPP_C__Katmanlı_Mimari_.Controllers
             int c = 2 * (kısa + uzun);
             return c;
         }
+        int Faktöriyel() 
+        {
+            int f= 1;
+            int sayi = 5;
+            for(int i = sayi; i > 1; i--) 
+            {
+                f = f * i;
+
+            }
+            return f;
+            
+        }
+
+        //string metotlar
+        string cumle() 
+        {
+            string c = "küçük hanımlar küçük beyler sizler hepiniz geleceğin birer gülü,parlayan," +
+                "ışık saçan yıldızı olacaksınız.";
+            return c;
+        }
+        public IActionResult Musteriler() 
+        {
+
+            ViewBag.d = cumle();
+            return View();
+        }
+
+        //paremetre kullanımı string
+        void mesajListesi(string p)
+        {
+            ViewBag.mesage = p;
+        }
+        //paremetre kullanımı int
+        int topla(int s1,int s2)
+        {
+            int t=s1 + s2;
+            return t;
+        }
 
         public IActionResult Index() 
         { 
             mesajlar();
+            mesajListesi("Parametre İsmi");
+            ViewBag.tp = topla(5, 5);
             return View();
         }
 
@@ -48,6 +88,10 @@ namespace Proje_OPP_C__Katmanlı_Mimari_.Controllers
             mesajlar(); 
             ViewBag.t=topla();
             ViewBag.c=cevre();
+            ViewBag.F=Faktöriyel();
+           
+      
+           
             return View();
         }
 
