8dc0fc5c06abe43083b0ab173de183b17de13afb
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-24 15:29:10 +0300

    Yeni Ürün Ekleme

--- Proje_OPP(C#_Katmanlı_Mimari)/Controllers/ProductController.cs
+++ Proje_OPP(C#_Katmanlı_Mimari)/Controllers/ProductController.cs
@@ -1,15 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OPP_C__Katmanlı_Mimari_.Entity;
 using Proje_OPP_C__Katmanlı_Mimari_.ProjeContext;
 
 namespace Proje_OPP_C__Katmanlı_Mimari_.Controllers
 {
     public class ProductController : Controller
     {
+        Context context = new Context();
         public IActionResult Index()
         {
-            Context context = new Context();
+           
             var values=context.Products.ToList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddProduct() 
+        { 
+        
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product p) 
+        {
+            context.Add(p);   
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
