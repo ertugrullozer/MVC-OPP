b8c55cfa6ca26d7c1eabad26455635bab5718a34
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-24 14:50:58 +0300

    Product List

---
+++ Proje_OPP(C#_Katmanlı_Mimari)/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OPP_C__Katmanlı_Mimari_.ProjeContext;
+
+namespace Proje_OPP_C__Katmanlı_Mimari_.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            Context context = new Context();
+            var values=context.Products.ToList();
+            return View(values);
+        }
+    }
+}
