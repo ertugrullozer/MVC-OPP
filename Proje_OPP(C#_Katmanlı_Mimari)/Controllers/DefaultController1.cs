821cf19e827014dc1417bbfe3e90fa9f0189c06d
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-24 12:29:35 +0300

    Ge

--- Proje_OPP(C#_Katmanlı_Mimari)/Controllers/DefaultController1.cs
+++ Proje_OPP(C#_Katmanlı_Mimari)/Controllers/DefaultController1.cs
@@ -19,10 +19,39 @@ namespace Proje_OPP_C__Katmanlı_Mimari_.Controllers
             ViewBag.m2 = "merhaba proje denemeleri";
             ViewBag.m3 = "selamlar hi hello bonjour";
         }
+        //Geriye değer döndüren metotlar
+
+        int topla()
+        {
+            int s1 = 20;
+            int s2 = 20;
+            int sonuc =s1 + s2;
+            return sonuc;
+        }
+
+        int cevre() 
+        {
+            int kısa = 10;
+            int uzun= 20;
+            int c = 2 * (kısa + uzun);
+            return c;
+        }
+
         public IActionResult Index() 
         { 
             mesajlar();
             return View();
         }
+
+        public IActionResult Urunler() 
+        {
+            mesajlar(); 
+            ViewBag.t=topla();
+            ViewBag.c=cevre();
+            return View();
+        }
+
+       
+
     }
 }
