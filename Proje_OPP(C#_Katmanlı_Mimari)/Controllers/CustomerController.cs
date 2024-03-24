bdb8ca0508a2367c6eaf6c872a1912fe68fc9133
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-24 16:49:40 +0300

    CRUD İşlemlerinin Yapılması

---
+++ Proje_OPP(C#_Katmanlı_Mimari)/Controllers/CustomerController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OPP_C__Katmanlı_Mimari_.Entity;
+using Proje_OPP_C__Katmanlı_Mimari_.ProjeContext;
+
+namespace Proje_OPP_C__Katmanlı_Mimari_.Controllers
+{
+    public class CustomerController : Controller
+    {
+      Context context = new Context();
+        public IActionResult Index()
+        {
+            var values = context.Customers.ToList();
+            return View(values);
+        }
+        [HttpGet] 
+        public IActionResult AddCustomer() 
+        {
+         return View();
+        }
+
+        [HttpPost] 
+        public IActionResult AddCustomer(Customer p) 
+        {
+            context.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteCustomer(int id) 
+        {
+            var delete = context.Customers.Where(x=>x.CustomerId == id).FirstOrDefault();
+            context.Remove(delete);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateCustomer(int id) 
+        {
+            var value = context.Customers.Where(x=>x.CustomerId==id).FirstOrDefault();
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer p)
+        {
+            var value = context.Customers.Where(x=>x.CustomerId==p.CustomerId).FirstOrDefault();
+            value.CustomerName= p.CustomerName;
+            value.City= p.City;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
